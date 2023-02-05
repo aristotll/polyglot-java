64b5f4a0bd362103ec20659a53114a8c53656a64
cheng.yao <cheng.yao@yijinin.com>
2023-02-05 22:06:34 +0800

    ignore unwanted modules

--- all/src/main/java/polyglot/All.java
+++ all/src/main/java/polyglot/All.java
@@ -4,16 +4,16 @@ public class All {
 	public void greet() {
 		System.out.println("Hello, I am Java!");
 		new HelloClojure().hello("Java");
-		new HelloFrege().hello("Java");
-		new HelloGosu().hello("Java");
-		new HelloGroovy().hello("Java");
-		new HelloHoot().hello("Java");
-		new HelloJava().hello("Java");
-		new HelloKotlin().hello("Java");
-		new HelloPython().hello("Java");
-		new HelloRuby().hello("Java");
-		new HelloScala().hello("Java");
-		new HelloXtend().hello("Java");
+//		new HelloFrege().hello("Java");
+//		new HelloGosu().hello("Java");
+//		new HelloGroovy().hello("Java");
+//		new HelloHoot().hello("Java");
+//		new HelloJava().hello("Java");
+//		new HelloKotlin().hello("Java");
+//		new HelloPython().hello("Java");
+//		new HelloRuby().hello("Java");
+//		new HelloScala().hello("Java");
+//		new HelloXtend().hello("Java");
 	}
 
 	public static void main(String[] args) {
